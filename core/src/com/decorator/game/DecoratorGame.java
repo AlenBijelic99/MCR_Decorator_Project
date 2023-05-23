9f87750f8699269fa328b5eee42577846d15f112
Alen Bijelic <alen.bijelic@heig-vd.ch>
2023-05-23 18:54:01 +0200

    WIP Upload tmx map and try to center

--- core/src/com/decorator/game/DecoratorGame.java
+++ core/src/com/decorator/game/DecoratorGame.java
@@ -1,31 +1,72 @@
 package com.decorator.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class DecoratorGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+	private OrthographicCamera camera;
+	private Viewport viewport;
+	private SpriteBatch batch;
+	private TiledMap tiledMap;
+	private TiledMapRenderer tiledMapRenderer;
+
 	@Override
-	public void create () {
+	public void create() {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		camera = new OrthographicCamera();
+		viewport = new FitViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), camera);
+
+		// Charger la carte créée avec Tiled
+		tiledMap = new TmxMapLoader().load("assets/testmap.tmx");
+		tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
+
+		// Ajuster le zoom de la caméra pour afficher la carte complète
+		float mapWidth = tiledMap.getProperties().get("width", Integer.class) * tiledMap.getProperties().get("tilewidth", Integer.class);
+		float mapHeight = tiledMap.getProperties().get("height", Integer.class) * tiledMap.getProperties().get("tileheight", Integer.class);
+
+		// Centrer la caméra sur la carte
+		camera.position.set(mapWidth / 2, mapHeight / 2, 0);
 	}
 
 	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	public void render() {
+		// Effacer l'écran avec une couleur de fond
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		// Mettre à jour la caméra et le viewport
+		camera.update();
+		batch.setProjectionMatrix(camera.combined);
+
+		// Afficher la carte
+		tiledMapRenderer.setView(camera);
+		tiledMapRenderer.render();
 	}
-	
+
 	@Override
-	public void dispose () {
+	public void resize(int width, int height) {
+		viewport.update(width, height);
+
+		// Ajuster le zoom de la caméra lors du redimensionnement de la fenêtre
+		float mapWidth = tiledMap.getProperties().get("width", Integer.class) * tiledMap.getProperties().get("tilewidth", Integer.class);
+		float mapHeight = tiledMap.getProperties().get("height", Integer.class) * tiledMap.getProperties().get("tileheight", Integer.class);
+
+		// Centrer la caméra sur la carte
+		camera.position.set(mapWidth / 2, mapHeight / 2, 0);
+	}
+
+	@Override
+	public void dispose() {
 		batch.dispose();
-		img.dispose();
+		tiledMap.dispose();
 	}
 }
