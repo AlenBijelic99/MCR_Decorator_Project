040ab4176685bc75d7fbfbc190037609c8daa4e8
Alen Bijelic <alen.bijelic@heig-vd.ch>
2023-05-30 15:55:13 +0200

    WIP new map

--- core/src/com/decorator/game/utils/Constants.java
+++ core/src/com/decorator/game/utils/Constants.java
@@ -3,10 +3,9 @@ package com.decorator.game.utils;
 public class Constants {
   public static final float PPM = 32f;
   public static final float GRAVITY = -25f;
-  public static final float PLAYER_SPEED = 10f;
-
+  public static final float PLAYER_SPEED = 15f;
   public static final int MAX_JUMPING_COUNT = 1;
-  public static final float JUMPING_SPEED = 18f;
-  public static final float MAX_JUMPING_HEIGHT = 25f;
-  public static final String[] MAPS = {"testmap.tmx"};
+  public static final float JUMPING_SPEED = 25f;
+  public static final float MAX_JUMPING_HEIGHT = 30f;
+  public static final String[] MAPS = {"snow.tmx"};
 }
