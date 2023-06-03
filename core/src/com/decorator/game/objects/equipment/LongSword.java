ae2c3b4ebd4d7043bde59fb10947b1b699845ef8
TegestBogale <tegest.g.bogale@gmail.com>
2023-06-03 20:17:56 +0200

    add long sword

--- core/src/com/decorator/game/objects/equipment/LongSword.java
+++ core/src/com/decorator/game/objects/equipment/LongSword.java
@@ -1,2 +1,19 @@
-package com.decorator.game.objects.equipment;public class LongSword {
+package com.decorator.game.objects.equipment;
+
+public class LongSword extends Weapon {
+    public LongSword(Equipment equipment) {
+        super(equipment);
+    }
+
+    @Override
+    public String getDescription() {
+        return super.getDescription() + this.getClass().getSimpleName();
+    }
+
+    @Override
+    public int addPower() {
+        return super.addPower() + 100;
+    }
+
+
 }
