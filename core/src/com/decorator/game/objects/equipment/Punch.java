1d812de4c999df1c57b732e68e529d60a8eb65db
TegestBogale <tegest.g.bogale@gmail.com>
2023-06-03 20:16:55 +0200

    punch added

--- core/src/com/decorator/game/objects/equipment/Punch.java
+++ core/src/com/decorator/game/objects/equipment/Punch.java
@@ -1,2 +1,19 @@
-package com.decorator.game.objects.equipment;public class Punch {
+package com.decorator.game.objects.equipment;
+
+public class Punch extends Weapon{
+    public Punch(Equipment equipment) {
+        super(equipment);
+    }
+
+    @Override
+    public String getDescription() {
+        return super.getDescription() + this.getClass().getSimpleName();
+    }
+
+    @Override
+    public int addPower() {
+        return super.addPower() + 20;
+    }
+
+
 }
