a22132b1adfda918e638d8637087638ff317e181
TegestBogale <tegest.g.bogale@gmail.com>
2023-06-03 20:18:42 +0200

    Kevlar armor

--- core/src/com/decorator/game/objects/equipment/KevlarArmor.java
+++ core/src/com/decorator/game/objects/equipment/KevlarArmor.java
@@ -1,2 +1,17 @@
-package com.decorator.game.objects.equipment;public class KevlarArmor {
+package com.decorator.game.objects.equipment;
+
+public class KevlarArmor extends Armor{
+    public KevlarArmor(Equipment equipment) {
+        super(equipment);
+    }
+
+    @Override
+    public String getDescription() {
+        return super.getDescription() + this.getClass().getSimpleName();
+    }
+
+    @Override
+    public int addDefense() {
+        return super.addDefense() + 30;
+    }
 }
