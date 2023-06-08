02e1c5304bfe17512d1e2ef0003463c3e743f777
Alen Bijelic <alen.bijelic@heig-vd.ch>
2023-06-08 20:02:03 +0200

    WIP: Create base player animation

--- core/src/com/decorator/game/objects/player/Player.java
+++ core/src/com/decorator/game/objects/player/Player.java
@@ -15,7 +15,7 @@ import com.decorator.game.utils.Constants;
 
 
 public class Player extends MovableGameEntity {
-  public enum State {FALLING, JUMPING, IDLE, RUNNING, ATTACKING};
+  public enum State {JUMPING, IDLE, RUNNING, ATTACKING, DEAD};
   public State currentState;
   public State previousState;
   private float stateTimer;
@@ -23,10 +23,17 @@ public class Player extends MovableGameEntity {
   private final Animation<TextureRegion> idle;
   private final Animation<TextureRegion> run;
   private final Animation<TextureRegion> jump;
+  private final Animation<TextureRegion> attack;
+  private final Animation<TextureRegion> dead;
   private boolean isRunningRight;
   private Equipment equipment;
   private float jumpHeight;
   private int strength;
+  private boolean isRunning;
+  private boolean isAttacking;
+  private boolean isDead;
+  private boolean isDeadAnimationFinished;
+
 
   public Player(float width, float height, Body body) {
     super(width, height, body);
@@ -38,24 +45,36 @@ public class Player extends MovableGameEntity {
     stateTimer = 0;
     isRunningRight = true;
 
-    Array<TextureRegion> frames = new Array<TextureRegion>();
+    Array<TextureRegion> frames = new Array<>();
 
-    for (int i = 0; i < 3; ++i){
-      frames.add(new TextureRegion(new Texture("player/player-idle-" + i + ".png")));
+    for (int i = 0; i < 1; ++i){
+      frames.add(new TextureRegion(new Texture("player/idle/Player_None_None_" + i + ".png")));
     }
-    idle = new Animation<TextureRegion>(0.3f, frames);
+    idle = new Animation<>(0.3f, frames);
     frames.clear();
 
     for (int i = 0; i < 9; ++i){
-      frames.add(new TextureRegion(new Texture("player/player-run-" + i + ".png")));
+      frames.add(new TextureRegion(new Texture("player/run/Player_None_None_" + i + ".png")));
     }
-    run = new Animation<TextureRegion>(0.1f, frames, Animation.PlayMode.LOOP);
+    run = new Animation<>(0.1f, frames, Animation.PlayMode.LOOP);
     frames.clear();
 
-    for (int i = 0; i < 3; ++i){
-      frames.add(new TextureRegion(new Texture("player/player-idle-" + i + ".png")));
+    for (int i = 0; i < 1; ++i){
+      frames.add(new TextureRegion(new Texture("player/idle/Player_None_None_" + i + ".png")));
     }
-    jump = new Animation<TextureRegion>(0.3f, frames);
+    jump = new Animation<>(0.3f, frames);
+    frames.clear();
+
+    for (int i = 0; i < 11; ++i){
+      frames.add(new TextureRegion(new Texture("player/attack/Player_None_None_" + i + ".png")));
+    }
+    attack = new Animation<>(0.06f, frames);
+    frames.clear();
+
+    for (int i = 0; i < 5; ++i){
+      frames.add(new TextureRegion(new Texture("player/dead/Player_None_None_" + i + ".png")));
+    }
+    dead = new Animation<>(0.3f, frames);
     frames.clear();
   }
 
@@ -87,21 +106,44 @@ public class Player extends MovableGameEntity {
 
   private void checkUserInput() {
     dx = 0;
-    if (Gdx.input.isKeyPressed(Input.Keys.A) || Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-      dx = -1;
-    } else if (Gdx.input.isKeyPressed(Input.Keys.D) || Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-      dx = 1;
+    if (!isDead) {
+      if (Gdx.input.isKeyPressed(Input.Keys.A) || Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+        dx = -1;
+        isRunning = true;
+      } else if (Gdx.input.isKeyPressed(Input.Keys.D) || Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+        dx = 1;
+        isRunning = true;
+      } else {
+        isRunning = false;
+      }
+
+      if ((Gdx.input.isKeyJustPressed(Input.Keys.W) || Gdx.input.isKeyJustPressed(Input.Keys.UP)) && jumpCount < Constants.MAX_JUMPING_COUNT) {
+        ++jumpCount;
+        body.setLinearVelocity(body.getLinearVelocity().x, 0);
+        body.applyLinearImpulse(new Vector2(0, body.getMass() * Constants.JUMPING_SPEED), body.getPosition(), true);
+      }
+
+      if (body.getLinearVelocity().y == 0) {
+        jumpCount = 0;
+      }
+
+      if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
+        isAttacking = true;
+      }
+    } else {
+      isRunning = false;
+      isAttacking = false;
     }
 
-    if ((Gdx.input.isKeyJustPressed(Input.Keys.W) || Gdx.input.isKeyJustPressed(Input.Keys.UP)) && jumpCount < Constants.MAX_JUMPING_COUNT) {
-      ++jumpCount;
-      body.setLinearVelocity(body.getLinearVelocity().x, 0);
-      body.applyLinearImpulse(new Vector2(0, body.getMass() * Constants.JUMPING_SPEED), body.getPosition(), true);
+    if (Gdx.input.isKeyJustPressed(Input.Keys.X)) {
+      if (!isDead) {
+        isDead = true;
+        isDeadAnimationFinished = false;
+      } else if (isDeadAnimationFinished) {
+        // Restart the game or perform any other action
+      }
     }
 
-    if (body.getLinearVelocity().y == 0) {
-      jumpCount = 0;
-    }
 
     body.setLinearVelocity(dx * speed, Math.min(body.getLinearVelocity().y, jumpHeight));
   }
@@ -113,12 +155,15 @@ public class Player extends MovableGameEntity {
     else if (body.getLinearVelocity().y < 0){
       return State.FALLING;
     }*/
-    else if (body.getLinearVelocity().x != 0){
+    else if (isRunning){
       return State.RUNNING;
-    }/*
-    else if (Gdx.input.isKeyPressed(Input.Keys.SPACE)){
+    }
+    else if (isAttacking){
       return State.ATTACKING;
-    }*/
+    }
+    else if (isDead){
+      return State.DEAD;
+    }
     else
       return State.IDLE;
   }
@@ -128,22 +173,36 @@ public class Player extends MovableGameEntity {
     currentState = getState();
     TextureRegion region;
 
-    switch (currentState){
+    switch (currentState) {
       case JUMPING:
         region = jump.getKeyFrame(stateTimer);
         break;
       case RUNNING:
         region = run.getKeyFrame(stateTimer, true);
         break;
+      case ATTACKING:
+        region = attack.getKeyFrame(stateTimer);
+        if (attack.isAnimationFinished(stateTimer)) {
+          currentState = State.IDLE;
+        }
+        break;
+      case DEAD:
+        region = dead.getKeyFrame(stateTimer);
+        if (dead.isAnimationFinished(stateTimer)) {
+          if (!isDeadAnimationFinished) {
+            isDeadAnimationFinished = true;
+            stateTimer -= dt; // Subtract the delta time to keep the last frame
+          }
+        }
+        break;
       default:
         region = idle.getKeyFrame(stateTimer, true);
     }
-    //if player is running left and the texture isn't facing left... flip it.
+    // Flip player if he is running left
     if ((body.getLinearVelocity().x < 0 || !isRunningRight) && !region.isFlipX()) {
       region.flip(true, false);
       isRunningRight = false;
     }
-    //if player is running right and the texture isn't facing right... flip it.
     else if((body.getLinearVelocity().x > 0 || isRunningRight) && region.isFlipX()){
       region.flip(true, false);
       isRunningRight = true;
