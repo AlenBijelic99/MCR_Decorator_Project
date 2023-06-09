7283b5fdf2c8c2a562f8ae466703b2572d070fcd
TegestBogale <tegest.g.bogale@gmail.com>
2023-06-10 00:52:43 +0200

    added weapon Entities

---
+++ core/src/com/decorator/game/objects/player/WeaponEntity.java
@@ -0,0 +1,28 @@
+package com.decorator.game.objects.player;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.decorator.game.objects.GameEntity;
+
+public abstract class WeaponEntity extends GameEntity implements Image{
+
+    public WeaponEntity(float x, float y,float width, float height, Body body) {
+        super(width, height, body);
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public void render(SpriteBatch batch) {
+        batch.begin();
+        String s = getImagePath();
+        TextureRegion region = new TextureRegion(new Texture(getImagePath()));
+        batch.draw(region, x - 10, y, region.getRegionWidth() * 4, region.getRegionHeight() * 4);
+        batch.end();
+    }
+    @Override
+    public void update() {
+    }
+}
