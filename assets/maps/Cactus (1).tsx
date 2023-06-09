2d89d5a5b8408129b98be7328e7bdae31e7419d6
TegestBogale <tegest.g.bogale@gmail.com>
2023-06-10 00:43:31 +0200

    [WIP] created map

---
+++ assets/maps/Cactus (1).tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="Cactus (1)" tilewidth="32" tileheight="32" tilecount="9" columns="3">
+ <image source="../deserttileset/png/Objects/Cactus (1).png" width="108" height="111"/>
+</tileset>
---
+++ assets/maps/stars.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="stars" tilewidth="32" tileheight="32" tilecount="620" columns="31">
+ <image source="../backgroundImage/stars.png" width="1000" height="667"/>
+</tileset>
