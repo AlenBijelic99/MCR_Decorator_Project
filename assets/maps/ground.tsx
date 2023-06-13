2e7a975fe280dc40c528105600b56daf1068a68f
TegestBogale <tegest.g.bogale@gmail.com>
2023-06-13 19:48:37 +0200

    small fix on map

--- assets/maps/ground.tsx
+++ assets/maps/ground.tsx
@@ -1,2 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="sanddesertGround" tilewidth="1" tileheight="1" tilecount="0" columns="0"/>
+<tileset version="1.10" tiledversion="1.10.1" name="sanddesertGround" tilewidth="1919" tileheight="293" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="1919" height="293" source="../backgroundImage/sandDesertGround.JPG"/>
+ </tile>
+</tileset>
