ef84b5640f34a550004a4836b0b9e6b2d0fa44ab
TegestBogale <tegest.g.bogale@gmail.com>
2023-06-18 16:39:22 +0200

    cleaned & commented armorEntity

--- core/src/com/decorator/game/objects/player/armorEntity/GoldArmorEntity.java
+++ core/src/com/decorator/game/objects/player/armorEntity/GoldArmorEntity.java
@@ -1,13 +1,33 @@
 package com.decorator.game.objects.player.armorEntity;
 
 import com.badlogic.gdx.physics.box2d.Body;
-import com.decorator.game.objects.player.armorEntity.ArmorEntity;
 
+/**
+ * Represents a gold armor entity.
+ *
+ * @author : Bijelic Alen, Bogale Tegest , Gillioz Dorian
+ * @version : 11.0.12
+ * @since : 17.05.2023
+ */
 public class GoldArmorEntity extends ArmorEntity {
+    /**
+     * Constructor with arguments.
+     *
+     * @param x      x coordinate
+     * @param y      y coordinate
+     * @param width  width
+     * @param height height
+     * @param body   body
+     */
     public GoldArmorEntity(float x, float y, float width, float height, Body body) {
         super(x, y, width, height, body);
     }
 
+    /**
+     * Get image path.
+     *
+     * @return image path
+     */
     @Override
     public String getImagePath() {
         return "assets/armor/gold.png";
--- core/src/com/decorator/game/objects/player/armorEntity/SilverArmorEntity.java
+++ core/src/com/decorator/game/objects/player/armorEntity/SilverArmorEntity.java
@@ -1,13 +1,33 @@
 package com.decorator.game.objects.player.armorEntity;
 
 import com.badlogic.gdx.physics.box2d.Body;
-import com.decorator.game.objects.player.armorEntity.ArmorEntity;
 
+/**
+ * Represents a silver armor entity.
+ *
+ * @author : Bijelic Alen, Bogale Tegest , Gillioz Dorian
+ * @version : 11.0.12
+ * @since : 17.05.2023
+ */
 public class SilverArmorEntity extends ArmorEntity {
+    /**
+     * Constructor with arguments.
+     *
+     * @param x      x coordinate
+     * @param y      y coordinate
+     * @param width  width
+     * @param height height
+     * @param body   body
+     */
     public SilverArmorEntity(float x, float y, float width, float height, Body body) {
         super(x, y, width, height, body);
     }
 
+    /**
+     * Get image path.
+     *
+     * @return image path
+     */
     @Override
     public String getImagePath() {
         return "assets/armor/silver.png";
