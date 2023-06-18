eb6d6fdb3e5717cd71392ad5ba3cd9c7247ab702
Alen Bijelic <alen.bijelic@heig-vd.ch>
2023-06-18 21:01:05 +0200

    Resize main window

--- desktop/src/com/decorator/game/DesktopLauncher.java
+++ desktop/src/com/decorator/game/DesktopLauncher.java
@@ -10,7 +10,7 @@ public class DesktopLauncher {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setTitle("DecoratorGame");
-		config.setFullscreenMode(Lwjgl3ApplicationConfiguration.getDisplayMode());
+		config.setWindowSizeLimits(1920, 950, 1920, 950);
 		new Lwjgl3Application(new DecoratorGame(), config);
 	}
 }
