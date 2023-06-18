61115a252c3ea0aa520b1a8428f72d8f871f4aa7
Alen Bijelic <alen.bijelic@heig-vd.ch>
2023-06-18 15:09:20 +0200

    Remove useless method

--- core/src/com/decorator/game/objects/equipment/potion/Potion.java
+++ core/src/com/decorator/game/objects/equipment/potion/Potion.java
@@ -22,15 +22,4 @@ public class Potion extends EquipmentDecorator {
     public Equipment getEquipment() {
         return super.getEquipment();
     }
-
-    @Override
-    public Equipment removeEquipment(Class<Equipment> c) {
-        if (equipment.getClass() == c) {
-            System.out.println("Removing " + equipment.getClass().getSimpleName());
-            equipment = equipment.getEquipment();
-        } else if (equipment instanceof EquipmentDecorator) {
-            equipment.removeEquipment(c);
-        }
-        return equipment;
-    }
 }
