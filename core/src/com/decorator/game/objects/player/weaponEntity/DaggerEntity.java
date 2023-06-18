8c440542be78ed9bd99a96c1adec08ba7e80e2d2
TegestBogale <tegest.g.bogale@gmail.com>
2023-06-18 16:42:23 +0200

    clead & commented weaponEntity

--- core/src/com/decorator/game/objects/player/weaponEntity/DaggerEntity.java
+++ core/src/com/decorator/game/objects/player/weaponEntity/DaggerEntity.java
@@ -2,10 +2,32 @@ package com.decorator.game.objects.player.weaponEntity;
 
 import com.badlogic.gdx.physics.box2d.Body;
 
+/**
+ * Represents a dagger entity.
+ *
+ * @author : Bijelic Alen, Bogale Tegest , Gillioz Dorian
+ * @version : 11.0.12
+ * @since : 17.05.2023
+ */
 public class DaggerEntity extends WeaponEntity {
-       public DaggerEntity(float x, float y, float width, float height, Body body) {
+    /**
+     * Constructor that creates a dagger entity.
+     *
+     * @param x      x coordinate
+     * @param y      y coordinate
+     * @param width  width
+     * @param height height
+     * @param body   body
+     */
+    public DaggerEntity(float x, float y, float width, float height, Body body) {
         super(x, y, width, height, body);
     }
+
+    /**
+     * Returns the image path of the dagger entity.
+     *
+     * @return image path
+     */
     @Override
     public String getImagePath() {
         return "weapons/Dagger.png";
